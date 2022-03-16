07ed55de37d57e76cf28c2ead575220fe8ba0552
SimonLin910709 <simon.lin910709@gmail.com>
2022-03-17 03:14:26 +0800

    part 3 completed

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ class Student extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      studentList: []
+      studentList: [],
+      searchName: ''
     };
   }
 
@@ -13,9 +14,12 @@ class Student extends Component {
     fetch('https://api.hatchways.io/assessment/students')
       .then(res => res.json())
       .then(res => {
-        console.log(res);
         this.setState({ studentList: res.students });
       })
+  }
+
+  onChangeSearchName = (event) => {
+    this.setState({ searchName: event.target.value });
   }
 
   getStudentAverage = (student) => {
@@ -24,24 +28,35 @@ class Student extends Component {
     return sum / student.grades.length;
   }
 
+  getStudentList = () => {
+    const { studentList, searchName } = this.state;
+    if (!searchName) return studentList;
+    return studentList.filter(item => item.firstName.includes(searchName) || item.lastName.includes(searchName));
+  }
+
   render() {
-    const { studentList } = this.state;
+    const { searchName } = this.state;
 
     return (
-      <div className="student-list">
-        {
-          studentList.map(student =>
-            <div className="student" key={student.id}>
-              <img src={student.pic} className="avatar" alt={student.id} />
-              <div className="content">
-                <div className="name">{`${student.firstName} ${student.lastName}`}</div>
-                <div className="email">Email: {student.email}</div>
-                <div className="company">Company: {student.company}</div>
-                <div className="skill">Skill: {student.skill}</div>
-                <div className="average">Average: {this.getStudentAverage(student)}%</div>
+      <div className="student-content">
+        <div className="search-input-content">
+          <input placeholder="Search by name" value={searchName} onChange={this.onChangeSearchName} />
+        </div>
+        <div className="student-list">
+          {
+            this.getStudentList().map(student =>
+              <div className="student" key={student.id}>
+                <img src={student.pic} className="avatar" alt={student.id} />
+                <div className="content">
+                  <div className="name">{`${student.firstName} ${student.lastName}`}</div>
+                  <div className="email">Email: {student.email}</div>
+                  <div className="company">Company: {student.company}</div>
+                  <div className="skill">Skill: {student.skill}</div>
+                  <div className="average">Average: {this.getStudentAverage(student)}%</div>
+                </div>
               </div>
-            </div>
-          )}
+            )}
+        </div>
       </div>
     );
   }
